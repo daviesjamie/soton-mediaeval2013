343488aaba3588f53d344b15bd912ea2f23306ac
John Preston <gizmoguy1@gmail.com>
2013-06-28 15:24:38 +0100

    Created initial Maven project from openimaj-quickstart-archetype for Search and Hyperlinking task.

---
+++ SearchAndHyperlinking/mediaeval.searchandhyperlinking/src/test/java/org/openimag/mediaeval/searchandhyperlinking/AppTest.java
@@ -0,0 +1,18 @@
+package org.openimag.mediaeval.searchandhyperlinking;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigourous Test :-)
+     */
+	@Test
+    public void testApp() {
+        assertTrue( true );
+    }
+}
