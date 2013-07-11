9a6a16a4dc77ad7e2715176692cbbbb76dc41d3e
Jamie Davies <jamie@jamiedavies.me>
2013-07-11 13:01:55 +0100

    Merge branch 'master' of https://github.com/daviesjamie/soton-mediaeval2013

---
+++ SearchAndHyperlinking/mediaeval.searchandhyperlinking/src/main/java/org/openimag/mediaeval/searchandhyperlinking/SearchUtils.java
@@ -0,0 +1,71 @@
+package org.openimag.mediaeval.searchandhyperlinking;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import gov.sandia.cognition.math.matrix.mtj.DenseVector;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.client.solrj.response.TermsResponse;
+import org.apache.solr.client.solrj.response.TermsResponse.Term;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.util.NamedList;
+
+public abstract class SearchUtils {
+
+	public static SolrDocumentList queryServer(SolrServer server, String query, 
+											   int maxResults) throws SolrServerException {
+		SolrQuery solrQuery = new SolrQuery(query);
+		solrQuery.setRows(maxResults);
+		solrQuery.addField("score");
+		solrQuery.addField("*");
+
+		QueryResponse response = server.query(solrQuery);
+		
+		return response.getResults();
+	}
+	
+	public static MapVector getTermVector(SolrServer server, String id) throws SolrServerException {
+		SolrQuery solrQuery = new SolrQuery("id:" + id);
+		solrQuery.setRequestHandler("/tvrh");
+		solrQuery.setParam("tv.tf_idf", true);
+		
+		QueryResponse response = server.query(solrQuery);
+		
+		if (response.getResults().size() != 1) return null;
+	
+		MapVector tfidfVector = new MapVector();
+		Map<Object, Double> tfidfMap = tfidfVector.getMap();
+		
+		NamedList<Object> termVectors =
+				(NamedList<Object>) response.getResponse().get("termVectors");
+		for (Entry<String, Object> idEntry : termVectors) {
+			if (idEntry.getKey().equals(id)) {
+				for (Entry<String, Object> phraseEntry : (NamedList<Object>) idEntry.getValue()) {
+					if (phraseEntry.getKey().equals("phrase")) {
+						for (Entry<String, Object> termEntry : (NamedList<Object>) phraseEntry.getValue()) {
+							String key = termEntry.getKey();
+							Double val = (Double) ((NamedList<Object>) termEntry.getValue()).get("tf-idf");
+							
+							tfidfMap.put(key, val);
+						}
+					}
+				}
+			}
+		}
+		
+		return tfidfVector;
+	}
+	
+	public static void main(String[] args) throws SolrServerException {
+		SolrServer server = new HttpSolrServer("http://seurat:8983/solr");
+		
+		getTermVector(server, "20080402_212000_bbcfour_up_pompeii_trans_limsi_2");
+	}
+}
---
+++ SearchAndHyperlinking/mediaeval.searchandhyperlinking/src/main/java/org/openimag/mediaeval/searchandhyperlinking/TestResult.java
@@ -0,0 +1,43 @@
+package org.openimag.mediaeval.searchandhyperlinking;
+
+public class TestResult {
+	private String queryID;
+	private String program;
+	private Float  start;
+	private Float  end;
+	
+	public TestResult(String queryID, String program, Float start, Float end) {
+		super();
+		this.queryID = queryID;
+		this.program = program;
+		this.start = start;
+		this.end = end;
+	}
+	
+	public String getQueryID() {
+		return queryID;
+	}
+	public void setQueryID(String queryID) {
+		this.queryID = queryID;
+	}
+	public String getProgram() {
+		return program;
+	}
+	public void setProgram(String program) {
+		this.program = program;
+	}
+	public Float getStart() {
+		return start;
+	}
+	public void setStart(Float start) {
+		this.start = start;
+	}
+	public Float getEnd() {
+		return end;
+	}
+	public void setEnd(Float end) {
+		this.end = end;
+	}
+	
+	
+}
