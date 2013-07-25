99f7943f9e6cf2b20c04c549fd63c3ba0638aac3
Dave Dupplaw <dpd@ecs.soton.ac.uk>
2013-07-25 16:14:34 +0100

    Updates to VideoTracker to store shot boundaries, write track files and better "best face" heuristic

--- SearchAndHyperlinking/mediaeval.searchhyper2013/src/main/java/org/openimaj/mediaeval/searchhyper2013/PersonRecogniser.java
+++ SearchAndHyperlinking/mediaeval.searchhyper2013/src/main/java/org/openimaj/mediaeval/searchhyper2013/PersonRecogniser.java
@@ -10,10 +10,12 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.imageio.ImageIO;
@@ -30,8 +32,11 @@ import opennlp.tools.util.Span;
 import org.openimaj.image.DisplayUtilities.ImageComponent;
 import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.MBFImage;
+import org.openimaj.image.processing.face.detection.DetectedFace;
+import org.openimaj.ml.annotation.ScoredAnnotation;
 import org.openimaj.util.api.auth.DefaultTokenFactory;
 import org.openimaj.util.api.auth.common.OpenCalaisAPIToken;
+import org.openimaj.util.pair.IndependentPair;
 
 import com.google.common.io.Files;
 import com.google.gson.JsonIOException;
@@ -53,14 +58,14 @@ public class PersonRecogniser
 	/** The person model for OpenNLP */
 	private static final String OPENNLP_PERSON_MODEL = "/org/openimaj/text/opennlp/models/en-ner-person.bin";
 
-	/**
-	 * The metadata directory where the programming information json files
-	 * reside
-	 */
+	/** The metadata directory where the programming information json files reside */
 	private String metadataDir = "/data/degas/mediaeval/mediaeval-searchhyper/collection/metadata/";
 
 	/** The directory where the videos reside */
 	private String videoDir = "/data/degas/mediaeval/mediaeval-searchhyper/collection/videos/";
+
+	/** The directory where the videos reside */
+	private String subtitlesDir = "/data/degas/mediaeval/mediaeval-searchhyper/collection/subtitles/xml/";
 
 	/**
 	 * The directory where the cached face images will be written to and read
@@ -112,8 +117,11 @@ public class PersonRecogniser
 	public void process() throws Exception
 	{
 		// Put the files together.
-		// First the programme metadata file
+		// First the programme metadata file...
 		final File programmeInfoFile = new File( this.metadataDir + this.baseFilename + ".json" );
+
+		// ...the subtitles file...
+		final File subtitlesFile = new File( this.subtitlesDir + this.baseFilename + ".xml" );
 
 		// .. and the video itself
 		final File videoFile = new File( this.videoDir + this.baseFilename + this.videoExtension );
@@ -123,22 +131,27 @@ public class PersonRecogniser
 			// Parse the programme metadata into a ProgrammeInfo object
 			final ProgrammeInfo pi = ProgrammeInfo.create( programmeInfoFile );
 
+			// Parse the programme subtitles into a ProgrammeSubtitles object
+			final ProgrammeSubtitles ps = ProgrammeSubtitles.create( subtitlesFile );
+
 			// Analyse the description for the people names
-			final Set<String> peopleNames = this.getPeopleNames( pi );
+			final Set<String> peopleNames = this.getPeopleNames( pi, ps );
+
+			System.out.println( "Found these people:\n\t"+peopleNames );
 
 			// Create a classifier for the given people names
-//			PersonMatcher pm = null;
-//			final File recFile = new File( this.baseFilename + ".rec" );
-//			if( recFile.exists() )
-//				pm = new PersonMatcher( recFile );
-//			else
-//				pm = new PersonMatcher( peopleNames, recFile, true );
-//
-//			// These options are for minimising false positives in images with
-//			// multiple people in them. We only have images with a single person
-//			// in it.
-//			pm.setAllowOnlyOneInstance( false );
-//			pm.setIgnoreBlurredFaces( false );
+			PersonMatcher pm = null;
+			final File recFile = new File( this.baseFilename + ".rec" );
+			if( recFile.exists() )
+				pm = new PersonMatcher( recFile );
+			else
+				pm = new PersonMatcher( peopleNames, recFile, true );
+
+			// These options are for minimising false positives in images with
+			// multiple people in them. We only have images with a single person
+			// in it.
+			pm.setAllowOnlyOneInstance( false );
+			pm.setIgnoreBlurredFaces( false );
 
 			// Images are being cached by the video tracker, so
 			// we don't need to do it if the directory for the given video
@@ -153,6 +166,7 @@ public class PersonRecogniser
 				vft.setStoreFaceImages( false );
 				vft.setCacheImages( true );
 				vft.setCacheDir( this.cacheDir );
+				vft.setnSkipFrames( 3 );
 
 				// We'll set the video aspect ratio
 				vft.setAspect( this.videoAspect );
@@ -181,23 +195,24 @@ public class PersonRecogniser
 			{
 				final MBFImage img = ImageUtilities.readMBF( f );
 
-//				final List<? extends IndependentPair<? extends DetectedFace, ScoredAnnotation<String>>> x = pm.query( img.flatten() );
+				final List<? extends IndependentPair<? extends DetectedFace, ScoredAnnotation<String>>> x
+						= pm.query( img.flatten() );
 
 				// As the query image should only have one face within it, the
 				// hope is that there will only be a single result in the person
 				// matcher.
-//				if( x.size() > 0 )
-//				{
-//					// Get the single result
-//					final IndependentPair<? extends DetectedFace, ScoredAnnotation<String>> y = x.get( 0 );
-//					final String name = (y.secondObject() == null ? "Unknown" : y.secondObject().annotation);
-//
-//					// Write the name of the person to a file next to the face
-//					// image.
-//					final FileWriter fw = new FileWriter( new File( f.getCanonicalPath() + ".txt" ) );
-//					fw.write( name );
-//					fw.close();
-//				}
+				if( x.size() > 0 )
+				{
+					// Get the single result
+					final IndependentPair<? extends DetectedFace, ScoredAnnotation<String>> y = x.get( 0 );
+					final String name = (y.secondObject() == null ? "Unknown" : y.secondObject().annotation);
+
+					// Write the name of the person to a file next to the face
+					// image.
+					final FileWriter fw = new FileWriter( new File( f.getCanonicalPath() + ".txt" ) );
+					fw.write( name );
+					fw.close();
+				}
 			}
 
 			this.showResults( files );
@@ -210,13 +225,58 @@ public class PersonRecogniser
 	}
 
 	/**
-	 * Given a {@link ProgrammeInfo}, it will analyse the description and return
+	 * 	Replaces all and numbers punctuation in a string with a space. Rather than removing
+	 * 	it replaces to avoid words merging (e.g. "david...john?" might become "davidjohn" instead
+	 * 	becomes "david    john ") and also keeps the character offsets the same in the text.
+	 *
+	 *	@param text The text
+	 *	@return
+	 */
+	public String cleanString( final String text )
+	{
+		return text.replaceAll("[^\\p{L} ']", " ");
+	}
+
+	/**
+	 * Given a {@link ProgrammeInfo} and {@link ProgrammeSubtitles},
+	 * it will analyse the description and return
 	 * a list of people names involved in the programme.
 	 *
 	 * @param pi The {@link ProgrammeInfo}
+	 * @param ps The {@link ProgrammeSubtitles}
 	 * @return A list of people names
 	 */
-	private Set<String> getPeopleNames( final ProgrammeInfo pi )
+	private Set<String> getPeopleNames( final ProgrammeInfo pi, final ProgrammeSubtitles ps )
+	{
+		final HashSet<String> people = new HashSet<String>();
+
+		people.addAll( this.getPeopleNames( this.cleanString( pi.description ) ) );
+
+		// For the subtitles text we will have to chop it down a bit.
+		final int maxLineLength = 1024;
+		final String[] lines = ps.getText().split( "\n." );
+		String currentLine = "";
+		for( final String line : lines )
+		{
+			if( currentLine.length() + line.length() < maxLineLength )
+					currentLine += this.cleanString( line )+"\n";
+			else
+			{
+				people.addAll( this.getPeopleNames( currentLine ) );
+				currentLine = this.cleanString( line )+"\n";
+			}
+		}
+
+		return people;
+	}
+
+	/**
+	 * 	Analyses the given text for people's names using both OpenCalais and OpenNLP.
+	 *
+	 *	@param text The text
+	 *	@return A list of people
+	 */
+	private Set<String> getPeopleNames( final String text )
 	{
 		final Set<String> peopleNames = new HashSet<String>();
 
@@ -226,11 +286,12 @@ public class PersonRecogniser
 			// Connect to the OpenCalais service
 			final OpenCalaisAPIToken token = DefaultTokenFactory.get( OpenCalaisAPIToken.class );
 			final CalaisClient cc = new CalaisRestClient( token.apiKey );
-			final CalaisResponse a = cc.analyze( pi.description );
+			final CalaisResponse a = cc.analyze( text );
 
 			// Get the names of the people out
 			for( final CalaisObject x : a.getEntities() )
-				peopleNames.add( x.getField( "commonname" ) );
+				if( x.getField( "commonname" ) != null )
+					peopleNames.add( x.getField( "commonname" ) );
 
 			// ----------------------------------------------------
 			// Now do the same for OpenNLP
@@ -244,7 +305,7 @@ public class PersonRecogniser
 				final NameFinderME finder = new NameFinderME( model );
 
 				// OpenNLP wants each token separately
-				final String[] splits = pi.description.split( "\\s" );
+				final String[] splits = text.split( "\\s" );
 
 				// Find people in the description
 				final Span[] spans = finder.find( splits );
@@ -254,7 +315,8 @@ public class PersonRecogniser
 				{
 					String name = "";
 					for( int x = span.getStart(); x < span.getEnd(); x++ )
-						name += splits[x] + " ";
+						if( splits[x] != null )
+							name += splits[x] + " ";
 
 					// Note that if it finds the same names as OpenCalais, then
 					// the HashSet will overwrite the name
@@ -406,6 +468,22 @@ public class PersonRecogniser
 				}
 			};
 		} );
+	}
+
+	/**
+	 *	@return the subtitlesDir
+	 */
+	public String getSubtitlesDir()
+	{
+		return this.subtitlesDir;
+	}
+
+	/**
+	 *	@param subtitlesDir the subtitlesDir to set
+	 */
+	public void setSubtitlesDir( final String subtitlesDir )
+	{
+		this.subtitlesDir = subtitlesDir;
 	}
 
 	/**
