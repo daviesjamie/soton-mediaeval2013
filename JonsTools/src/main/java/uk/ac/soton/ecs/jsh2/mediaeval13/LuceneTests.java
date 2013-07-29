1fc95d5467bd3a6d2645e8d6c8bd42cb325f8f72
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2013-07-29 10:26:47 +0100

    lucene indexer and searcher

--- JonsTools/src/main/java/uk/ac/soton/ecs/jsh2/mediaeval13/LuceneTests.java
+++ JonsTools/src/main/java/uk/ac/soton/ecs/jsh2/mediaeval13/LuceneTests.java
@@ -1,160 +1,55 @@
 package uk.ac.soton.ecs.jsh2.mediaeval13;
 
-import gnu.trove.list.array.TFloatArrayList;
-import gnu.trove.list.array.TLongArrayList;
-
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.atomic.AtomicInteger;
 
-import org.apache.commons.io.LineIterator;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.DateTools;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.Field.Store;
-import org.apache.lucene.document.IntField;
-import org.apache.lucene.document.LongField;
-import org.apache.lucene.document.StoredField;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.index.IndexableField;
-import org.apache.lucene.spatial.SpatialStrategy;
-import org.apache.lucene.spatial.prefix.RecursivePrefixTreeStrategy;
-import org.apache.lucene.spatial.prefix.tree.GeohashPrefixTree;
-import org.apache.lucene.spatial.prefix.tree.SpatialPrefixTree;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopScoreDocCollector;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.SimpleFSDirectory;
 import org.apache.lucene.util.Version;
-import org.openimaj.util.function.Operation;
-import org.openimaj.util.iterator.IterableIterator;
-import org.openimaj.util.parallel.GlobalExecutorPool.DaemonThreadFactory;
-import org.openimaj.util.parallel.Parallel;
-import org.openimaj.util.parallel.partition.FixedSizeChunkPartitioner;
-
-import com.spatial4j.core.context.SpatialContext;
-import com.spatial4j.core.shape.Shape;
+import org.openimaj.image.DisplayUtilities;
+import org.openimaj.image.FImage;
 
 public class LuceneTests {
-	private static final String FIELD_ID = "id";
-	private static final String FIELD_USER = "user";
-	private static final String FIELD_URL = "url";
-	private static final String FIELD_TAKEN = "taken";
-	private static final String FIELD_UPLOADED = "uploaded";
-	private static final String FIELD_VIEWS = "views";
+	public static void main(String[] args) throws IOException, ParseException {
+		final Directory directory = new SimpleFSDirectory(new File("/Users/jon/lucene-test-index"));
 
-	final static String CSV_REGEX = ",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))";
+		final Query q = new QueryParser(Version.LUCENE_43, "tags", new StandardAnalyzer(Version.LUCENE_43))
+				.parse("+southampton +snow");
 
-	public static void main(String[] args) throws IOException {
-		final String latlngPath = "/Volumes/SSD/training_latlng";
-		final String csvPath = "/Volumes/My Book/mediaeval-placing/all.csv";
+		// 3. search
+		final int hitsPerPage = 100000;
+		final IndexReader reader = DirectoryReader.open(directory);
+		final IndexSearcher searcher = new IndexSearcher(reader);
+		final TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage, true);
+		searcher.search(q, collector);
+		final ScoreDoc[] hits = collector.topDocs().scoreDocs;
 
-		final TLongArrayList ids = new TLongArrayList(7600000);
-		final TFloatArrayList lats = new TFloatArrayList(7600000);
-		final TFloatArrayList lons = new TFloatArrayList(7600000);
+		// 4. display results
+		System.out.println("Found " + collector.getTotalHits() + " hits.");
+		final FImage img = new FImage(360, 180);
+		for (int i = 0; i < hits.length; ++i) {
+			final int docId = hits[i].doc;
+			final Document d = searcher.doc(docId);
+			final String[] llstr = d.get("location").split(" ");
+			final float x = Float.parseFloat(llstr[0]) + 180;
+			final float y = 90 - Float.parseFloat(llstr[1]);
 
-		BufferedReader br = new BufferedReader(new FileReader(latlngPath));
-		String line;
-		br.readLine(); // skip first line
-		while ((line = br.readLine()) != null) {
-			final String[] parts = line.split(" ");
-
-			ids.add(Long.parseLong(parts[0]));
-			lats.add(Float.parseFloat(parts[1]));
-			lons.add(Float.parseFloat(parts[2]));
+			img.pixels[(int) (y % img.height)][(int) (x % img.width)] = 1;
 		}
-		br.close();
+		DisplayUtilities.display(img.normalise());
 
-		final SpatialContext ctx = SpatialContext.GEO;
-		final SpatialPrefixTree grid = new GeohashPrefixTree(ctx, 11);
-		final RecursivePrefixTreeStrategy strategy = new RecursivePrefixTreeStrategy(grid, "location");
-
-		final StandardAnalyzer a = new StandardAnalyzer(Version.LUCENE_43);
-		final IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_43, a);
-		iwc.setRAMBufferSizeMB(256);
-		Directory directory;
-		directory = new SimpleFSDirectory(new File("/Volumes/SSD/lucene-test-index-2"));
-		final IndexWriter indexWriter = new IndexWriter(directory, iwc);
-
-		final AtomicInteger counter = new AtomicInteger();
-		br = new BufferedReader(new FileReader(csvPath));
-		final LineIterator iter = new LineIterator(br);
-
-		final ThreadPoolExecutor pool = (ThreadPoolExecutor) Executors.newFixedThreadPool(4, new DaemonThreadFactory());
-		Parallel.forEachPartitioned(new FixedSizeChunkPartitioner<String>(IterableIterator.in(iter), 1000),
-				new Operation<Iterator<String>>() {
-					@Override
-					public void perform(Iterator<String> it) {
-						int bsize = 0;
-						while (it.hasNext()) {
-							final String line = it.next();
-							if (line.startsWith("photo"))
-								continue;
-
-							final String[] parts = line.split(CSV_REGEX);
-							// photoID,accuracy,userID,photoLink,photoTags,DateTaken,DateUploaded,views,licenseID
-							final long flickrId = Long.parseLong(parts[0]);
-
-							final int idx = ids.indexOf(flickrId);
-							if (idx < 0)
-								continue;
-
-							final String userId = parts[2];
-							final String url = parts[3];
-							final String tags = parts[4];
-							final Date taken = new Date(Long.parseLong(parts[5]));
-							final Date uploaded = new Date(Long.parseLong(parts[6]));
-							final int views = Integer.parseInt(parts[7]);
-
-							final Document doc = makeDocument(flickrId, userId, url, tags, taken, uploaded, views,
-									lats.get(idx),
-									lons.get(idx), ctx, strategy);
-							try {
-								indexWriter.addDocument(doc);
-							} catch (final IOException e) {
-								e.printStackTrace();
-							}
-
-							bsize++;
-						}
-						synchronized (LuceneTests.class) {
-							System.out.println(counter.addAndGet(bsize));
-						}
-					}
-				}, pool);
-		indexWriter.commit();
-		indexWriter.close();
-	}
-
-	@SuppressWarnings("deprecation")
-	private static Document makeDocument(long flickrId, String userId, String url, String tags, Date taken,
-			Date uploaded, int views, float lat, float lon, SpatialContext ctx, SpatialStrategy strategy)
-	{
-		final Document doc = new Document();
-
-		doc.add(new LongField(FIELD_ID, flickrId, Store.YES));
-		doc.add(new StringField(FIELD_USER, userId, Store.YES));
-		doc.add(new StringField(FIELD_URL, url, Store.YES));
-		doc.add(new TextField(FIELD_USER, tags, Store.NO));
-		doc.add(new StringField(FIELD_TAKEN, DateTools.dateToString(taken, DateTools.Resolution.MINUTE), Field.Store.YES));
-		doc.add(new StringField(FIELD_UPLOADED, DateTools.dateToString(uploaded, DateTools.Resolution.MINUTE),
-				Field.Store.YES));
-		doc.add(new IntField(FIELD_VIEWS, views, Store.YES));
-
-		final Shape point = ctx.makePoint(lon, lat);
-		for (final IndexableField f : strategy.createIndexableFields(point)) {
-			doc.add(f);
-		}
-		doc.add(new StoredField(strategy.getFieldName(), ctx.toString(point)));
-
-		return doc;
+		// reader can only be closed when there
+		// is no need to access the documents any more.
+		reader.close();
 	}
 }
