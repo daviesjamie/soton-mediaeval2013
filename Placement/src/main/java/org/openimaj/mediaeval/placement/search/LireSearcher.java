8fc81d3eaf328bcb373ddf25bcf8d1f69e0db90e
Jamie Davies <jamie@jamiedavies.me>
2013-08-15 09:57:38 +0100

    Fixed some stupidness in LireSearcher class.

--- Placement/src/main/java/org/openimaj/mediaeval/placement/search/LireSearcher.java
+++ Placement/src/main/java/org/openimaj/mediaeval/placement/search/LireSearcher.java
@@ -15,8 +15,8 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.NumericRangeQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
-import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.MBFImage;
 import org.openimaj.mediaeval.placement.data.LireFeatures;
@@ -26,49 +26,49 @@ import uk.ac.soton.ecs.jsh2.mediaeval13.placing.search.VisualSearcher;
 
 public class LireSearcher implements VisualSearcher {
 
-    private IndexReader index;
-    private ImageSearcher searcher;
-    private IndexSearcher meta;
+    private IndexReader ir;
+    private IndexSearcher metaSearch;
+    private ImageSearcher visSearch;
+    private IndexSearcher featSearch;
 
-    public LireSearcher( String lireIndex, LireFeatures feature, IndexSearcher meta, int maxHits ) throws IOException {
-        this.index = DirectoryReader.open( FSDirectory.open( new File( lireIndex )  ) );
-        this.meta = meta;
-
-        this.searcher = new GenericFastImageSearcher( maxHits, feature.fclass, feature.name );
+    public LireSearcher( String lireIndex, LireFeatures feature, IndexSearcher metaSearch, int maxHits ) throws IOException {
+        this.ir =  DirectoryReader.open( FSDirectory.open( new File( lireIndex ) ) );
+        this.featSearch = new IndexSearcher( ir );
+        this.visSearch = new GenericFastImageSearcher( maxHits, feature.fclass, feature.name );
+        this.metaSearch = metaSearch;
     }
 
     @Override
     public ScoreDoc[] search( MBFImage query, int numResults ) throws IOException {
-        BufferedImage bquery = ImageUtilities.createBufferedImageForDisplay( query );
-        DisplayUtilities.display( bquery, "Query image" );
-
-        ImageSearchHits hits = searcher.search( bquery, index );
-
+        BufferedImage bquery = ImageUtilities.createBufferedImage( query );
+        ImageSearchHits hits = visSearch.search( bquery, ir );
+        
         return linkResults( hits );
     }
 
     @Override
     public ScoreDoc[] search( long flickrId, int numResults ) throws IOException {
-        System.out.println( "Query: " + flickrId );
         final Query q = NumericRangeQuery.newLongRange( LuceneIndexBuilder.FIELD_ID, flickrId, flickrId, true, true );
-        int docId = meta.search( q, 1 ).scoreDocs[ 0 ].doc;
+        final TopDocs topdocs = featSearch.search( q, 1 );
+        final int docId = topdocs.scoreDocs[ 0 ].doc;
+        final Document dquery = featSearch.doc( docId );
         
-        ImageSearchHits hits = searcher.search( meta.doc( docId ), index );
+        ImageSearchHits hits = visSearch.search( dquery, ir );
         
         return linkResults( hits );
     }
-
+    
     private ScoreDoc[] linkResults( ImageSearchHits hits ) throws IOException {
         final ScoreDoc[] docs = new ScoreDoc[ hits.length() ];
-
-        for( int i = 0; i < docs.length; i++ ) {
+        
+        for( int i = 0; i < hits.length(); i++ ) {
             final Document hit = hits.doc( i );
             long photoId = Long.parseLong( hit.get( LuceneIndexBuilder.FIELD_ID ) );
             final Query q = NumericRangeQuery.newLongRange( LuceneIndexBuilder.FIELD_ID, photoId, photoId, true, true );
-            docs[ i ] = meta.search( q, 1 ).scoreDocs[ 0 ];
+            docs[ i ] = metaSearch.search( q, 1 ).scoreDocs[ 0 ];
             docs[ i ].score = hits.score( i );
         }
-
+        
         return docs;
     }
 
